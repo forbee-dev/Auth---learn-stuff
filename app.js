1568ad3fdeda6fdc5585ea487ce5c84681dd9893
ForBee.dev <code@forbee.dev>
2022-09-20 23:26:25 +0100

    modified:   app.js

--- app.js
+++ app.js
@@ -8,40 +8,36 @@ const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true});
 
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
-
-});
+const userSchema = new mongoose.Schema({email: String, password: String});
 
 const secret = "opequenosegredodaspasswords.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+userSchema.plugin(encrypt, {
+    secret: secret,
+    encryptedFields: ["password"]
+});
 
 const User = new mongoose.model("User", userSchema);
 
-app.get ("/", function (req, res) {
+app.get("/", function (req, res) {
     res.render("home");
 })
 
-app.get ("/login", function (req, res) {
+app.get("/login", function (req, res) {
     res.render("login");
 })
 
-app.get ("/register", function (req, res) {
+app.get("/register", function (req, res) {
     res.render("register");
 });
 
-app.post ("/register", function (req, res) {
-    const newUser = new User ({
-        email: req.body.username,
-        password: req.body.password
-    });
+app.post("/register", function (req, res) {
+    const newUser = new User(
+        {email: req.body.username, password: req.body.password}
+    );
 
     newUser.save(function (err) {
         if (err) {
@@ -52,24 +48,25 @@ app.post ("/register", function (req, res) {
     })
 });
 
-app.post ("/login", function (req, res) {
+app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email: username}, function (err, foundUser) {
+    User.findOne({
+        email: username
+    }, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password === password)
-                res.render("secrets");
+                if (foundUser.password === password) 
+                    res.render("secrets");
+                }
             }
-        }
     })
 })
 
-
 app.listen(3000, function () {
     console.log("Server Started");
-    
+
 })
