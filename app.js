6a5bf6d98966d5ccbea16c373b329f5d97b20f37
ForBee.dev <code@forbee.dev>
2022-09-21 14:39:40 +0100

    modified:   Level 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -14,12 +14,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost:27017/usersDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({email: String, password: String});
-
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {
-    secret: secret,
-    encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -37,7 +31,7 @@ app.get("/register", function (req, res) {
 
 app.post("/register", function (req, res) {
     const newUser = new User(
-        {email: req.body.username, password: req.body.password}
+        {email: req.body.username, password: md5(req.body.password)}
     );
 
     newUser.save(function (err) {
@@ -51,7 +45,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({
         email: username
